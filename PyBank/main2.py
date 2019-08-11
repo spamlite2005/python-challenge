c37d9c4e1791f9fb0a6485b0ff3e2d2b73e0848e
spamlite2005 <spamlite2005@gmail.com>
2019-08-10 22:50:58 -0400

    2nd Pybank commit

--- PyBank/main2.py
+++ PyBank/main2.py
@@ -12,8 +12,16 @@ with open(csv_path) as csvfile:
 
     for row in csvreader:
         months = months + 1
-        profit_loss = int(row[1])
-    
+        profit_loss = profit_loss + int(row[1])
+        print(row[0], row[1])
+
     print(str(months))
     print(str(profit_loss))
+    avg_profit_loss = round(profit_loss / months, 2)
+    print(str(avg_profit_loss))
 
+    max_profit_loss = max(int(row[1]))
+    print(str(max_profit_loss))
+
+    min_profit_loss = min(int(row[1]))
+    print(str(min_profit_loss))
